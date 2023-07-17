730eefcf6fad02255f5f1269d8300b30d75627ec
MaiGaode <100212357+MaiGaode@users.noreply.github.com>
2023-07-17 18:09:01 +0800

    Add files via upload

---
+++ po/Hotel.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright (c) 2019.  admin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hyc.www.po;
+
+import com.hyc.www.po.abs.BaseEntity;
+
+import java.math.BigDecimal;
+
+/**
+ * @author admin
+ * @program XHotel
+ * @description 酒店实体类
+ */
+public class Hotel extends BaseEntity {
+    private String number;
+
+    private String name ;
+    private String site;
+    private String type;
+    private BigDecimal area;
+    private BigDecimal star;
+    private BigDecimal score;
+    private String introduction;
+    private String photo;
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSite() {
+        return site;
+    }
+
+    public void setSite(String site) {
+        this.site = site;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public BigDecimal getArea() {
+        return area;
+    }
+
+    public void setArea(BigDecimal area) {
+        this.area = area;
+    }
+
+    public BigDecimal getStar() {
+        return star;
+    }
+
+    public void setStar(BigDecimal star) {
+        this.star = star;
+    }
+
+    public BigDecimal getScore() {
+        return score;
+    }
+
+    public void setScore(BigDecimal score) {
+        this.score = score;
+    }
+
+    public String getIntroduction() {
+        return introduction;
+    }
+
+    public void setIntroduction(String introduction) {
+        this.introduction = introduction;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+}
